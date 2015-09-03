8b2c882c21f53964b75c8f182184cb97c9582135
Daniel Matthew <hello@danmatthew.co.uk>
2015-09-03 21:18:00 +0100

    Added ARIA roles to template; added colophon

--- gruntfile.js
+++ gruntfile.js
@@ -43,5 +43,7 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-imagemin');
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('watch', ['watch']);
 	grunt.registerTask('default', ['sass', 'imagemin']);
 };
