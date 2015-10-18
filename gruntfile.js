f62db0c2ce931dae74aaa4a30d6e3f07d32a394c
Daniel Matthew <hello@danmatthew.co.uk>
2015-10-18 19:06:26 +0100

    Update appearance

--- gruntfile.js
+++ gruntfile.js
@@ -1,49 +1,45 @@
+'use strict';
 module.exports = function(grunt) {
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-		imagemin: {
-			// static: {
-			// 	files: {
-			// 		'public/img/src/breakout.png': 'public/img/build/breakout.png'
-			// 	}
-			// }
-			dynamic: {
-				files: [{
-					expand: true,
-					cwd: 'public/img/src/',
-					src: ['**/*.jpg'],
-					dest: 'public/img/build/'
-				}]
-			}
-		},
+    imagemin: {
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: 'public/img/src/',
+          src: ['**/*.jpg'],
+          dest: 'public/img/build/'
+        }]
+      }
+    },
 
-		sass: {
-			dist: {
-				options: {
-					style: 'compressed'
-				},
-				files: {
-					'public/css/styles.css':'public/sass/styles.scss'
-				}
-			}
-		},
+    sass: {
+      dist: {
+        options: {
+          style: 'compressed'
+        },
+        files: {
+          'public/css/styles.css':'public/sass/styles.scss'
+        }
+      }
+    },
 
-		watch: {
-			css: {
-				files: ['public/sass/**/*.scss'],
-				tasks: ['sass'],
-				options: {
-					spawn: false
-				}
-			}
-		}
-	});
+    watch: {
+      css: {
+        files: ['public/sass/**/*.scss'],
+        tasks: ['sass'],
+        options: {
+          spawn: false
+        }
+      }
+    }
+  });
 
-	grunt.loadNpmTasks('grunt-contrib-imagemin');
-	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-	grunt.registerTask('watch', ['watch']);
-	grunt.registerTask('default', ['sass', 'imagemin']);
+  grunt.registerTask('watch', ['watch']);
+  grunt.registerTask('default', ['sass', 'imagemin']);
 };
