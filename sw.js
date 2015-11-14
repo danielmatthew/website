866c99c732e9b2bfc8fb0f9c61f4b28037a1052b
Daniel Matthew <hello@danmatthew.co.uk>
2015-11-14 18:17:11 +0000

    Remove request url logging

--- sw.js
+++ sw.js
@@ -12,7 +12,6 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  console.log(event.request.url);
   event.respondWith(
     caches.match(event.request).then(function(response) {
       return response || fetch(event.request);
