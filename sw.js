6af3f07b65d92c0ea59e3d6e77c9f6a1579fb425
Daniel Matthew <hello@danmatthew.co.uk>
2015-11-14 18:10:16 +0000

    Configure service worker

---
+++ sw.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+      caches.open('dm').then(function(cache) {
+        return cache.addAll([
+            '/',
+            '/index.html',
+            '/public/css/styles.css',
+            '/public/js/main.js'
+          ]);
+      })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log(event.request.url);
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
