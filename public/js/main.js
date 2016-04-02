be5ac8c7012d8e508a8c9278fae1fbc50cf5d571
Daniel Matthew <hello@danmatthew.co.uk>
2016-04-02 23:11:16 +0100

    Refactor service worker to load contents from manifest

--- public/js/main.js
+++ public/js/main.js
@@ -2,9 +2,23 @@
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('./sw.js')
       .then(function(reg) {
-        console.log(reg);
-      }).catch(function(err) {
-        console.log(err);
+        console.log('The service worker has been registered ', reg);
       })
+      .catch(function(err) {
+        console.log(err);
+      });
+
+      navigator.serviceWorker.addEventListener('controllerchange', function(event) {
+        console.log('[controllerchange] A "controllerchange" event has happened within navigator.serviceWorker: ', event);
+
+
+        navigator.serviceWorker.controller.addEventListener('statechange', function() {
+          console.log('[controllerchange][statechange] A "statechange" has occured: ', this.state);
+
+          if (this.state === 'activated') {
+            document.getElementById('offlineNotification').classList.remove('hidden');
+          }
+        });
+      });
   }
 }());
