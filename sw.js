a045d132d280b8a05b1cdafbf7f4521a311bb56d
Daniel Matthew <hello@danmatthew.co.uk>
2016-05-31 22:08:49 +0100

    Fix SW bugs

--- sw.js
+++ sw.js
@@ -1,17 +1,20 @@
+'use strict';
+
+const version = '2';
+
 self.addEventListener('install', event => {
   event.waitUntil(
-      caches.open('dm')
-        .then(cache => cache.addAll([
-            '/public/css/styles.css',
-            '/public/js/main.js'
-          ]))
-      })
+    caches.open(`static-${version}`)
+      .then(cache => cache.addAll([
+        '/public/css/styles.css',
+        '/public/js/main.js'
+      ]))
     );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request));
+      .then(response => response || fetch(event.request))
   );
 });
