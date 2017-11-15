3f7515400055262561ad51f3f3f502133c1be028
Dan Matthew <dm@talis.com>
2017-11-15 18:28:22 +0000

    Remove logging

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import Link from "gatsby-link";
 import Helmet from "react-helmet";
 
 export default ({ data }) => {
-  console.log(data);
   const { edges: posts } = data.allMarkdownRemark;
 
   return (
