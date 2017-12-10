eb34f1b542e1ca9e8c61b1880784782b2d8073cd
Dan Matthew <dm@talis.com>
2017-12-10 14:55:13 +0000

    Components

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Link from 'gatsby-link';
 import Helmet from 'react-helmet';
 
+import About from '../components/About';
+
 export default ({ data }) => {
   const siteTitle = data.site.siteMetadata.title;
   const { edges: posts } = data.allMarkdownRemark;
@@ -9,15 +11,28 @@ export default ({ data }) => {
   return (
     <div>
       <Helmet title={siteTitle} />
+      <About />
+      <h2>Recent posts</h2>
       {posts
         .filter(post => post.node.frontmatter.title.length > 0)
         .map(({ node: post }) => (
           <div key={post.id}>
             <Link to={post.fields.slug}>
-              <h2>{post.frontmatter.title}</h2>
+              <h3>{post.frontmatter.title}</h3>
             </Link>
           </div>
         ))}
+        <section>
+          <h2>Recent talks</h2>
+          <article>
+            <h3>Hydrahack, March 2017</h3>
+            <p></p>
+          </article>
+          <article>
+            <h3>Codelicious, July 2016</h3>
+            <p></p>
+          </article>
+        </section>
     </div>
   );
 };
