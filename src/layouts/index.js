acf21fccbf3943517bff7943f9e35e4ef050760f
Dan Matthew <github@danmatthew.co.uk>
2018-01-22 22:51:55 +0000

    Adds review of 2017

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -25,13 +25,12 @@ const TemplateWrapper = ({ children, location }) => (
         },
       ]}
     />
-    <Header location={location} />
+    <Header location={ location } />
     <div
       style={{
         margin: '0 auto',
         maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
+        padding: '0 1.0875rem 1.45rem',
       }}
     >
       {children()}
