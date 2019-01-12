c828316b4432831b74da46c50d4299a097503e0f
Dan Matthew <dm@talis.com>
2019-01-12 19:44:46 +0000

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 
-
-// import Header from '../components/Header';
-
 import './index.css';
 
 const TemplateWrapper = ({ children, location }) => (
