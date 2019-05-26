6a2e9f3760770d006906921f2a4703f3363ef5e6
Dan Matthew <dm@talis.com>
2019-05-26 17:57:02 +0100

    Update strap-line

--- src/components/Monogram.js
+++ src/components/Monogram.js
@@ -4,9 +4,9 @@ import './Monogram.css';
 const Monogram = () => {
   return (
     <h1 className="monogram">
-      <span className="sr-only">Daniel Matthew</span>
-      <span role="presentation">D</span>
-      <span role="presentation">M</span>
+      <span className="sr-only">Daniel Matthew: Web Accessibility Consultant</span>
+      <span role="presentation" aria-hidden="true">D</span>
+      <span role="presentation" aria-hidden="true">M</span>
     </h1>
   );
 };
