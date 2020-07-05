b149f891a38620e8d3d4974afefb95fff03a477e
Dan Matthew <dm@talis.com>
2020-07-05 09:26:23 +0100

    chore: bring back pass through CSS

--- .eleventy.js
+++ .eleventy.js
@@ -32,7 +32,7 @@ module.exports = function (config) {
   config.addPassthroughCopy('src/fonts');
   config.addPassthroughCopy('src/images');
   config.addPassthroughCopy('src/js');
-  // config.addPassthroughCopy('src/css');
+  config.addPassthroughCopy('src/css');
   config.addPassthroughCopy('src/admin/config.yml');
   config.addPassthroughCopy('src/admin/previews.js');
   config.addPassthroughCopy('node_modules/nunjucks/browser/nunjucks-slim.js');
