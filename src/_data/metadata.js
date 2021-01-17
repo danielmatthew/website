0369f53a24ec9af86dfcdcc3e8be8749ce76ec73
Dan Matthew <dm@talis.com>
2021-01-17 18:34:07 +0000

    chore: move dependencies

---
+++ src/_data/metadata.js
@@ -0,0 +1,11 @@
+const groq = require('groq');
+const client = require('../utils/sanityClient');
+
+module.exports =  async function() {
+  return await client.fetch(groq`
+    *[_id == "siteSettings"]{
+      ...,
+      author->
+    }[0]
+  `)
+}
---
+++ src/client-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  sanity: {
+    projectId: process.env.SANITY_PROJECT_ID || '06f3iyia',
+    dataset: process.env.SANITY_DATASET || 'production'
+  }
+}
