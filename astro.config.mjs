1f20f1be6690ea516e89080c4ed37a6d91b76f24
Dan Matthew <111794+danielmatthew@users.noreply.github.com>
2023-08-07 11:33:38 +0100

    chore: add mdx support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 // import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
-export default defineConfig();
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [mdx()],
+});
