49aa9ed37b09ecb579439859885cea74e958b9bc
Dan Matthew <111794+danielmatthew@users.noreply.github.com>
2023-08-09 18:28:57 +0100

    chore: support case studies and articles

--- src/content/config.js
+++ src/content/config.js
@@ -1,14 +1,29 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 export const collections = {
-	work: defineCollection({
-		schema: z.object({
-			title: z.string(),
-			description: z.string(),
-			publishDate: z.coerce.date(),
-			tags: z.array(z.string()),
-			img: z.string(),
-			img_alt: z.string().optional(),
-		}),
-	}),
+  work: defineCollection({
+    schema: z.object({
+      title: z.string(),
+      description: z.string(),
+      projectDate: z.string().optional(),
+      publishDate: z.coerce.date(),
+      tags: z.array(z.string()),
+      img: z.string(),
+      img_alt: z.string().optional(),
+    }),
+  }),
+  posts: defineCollection({
+    schema: z.object({
+      title: z.string()
+    }),
+  }),
+  articles: defineCollection({
+    schema: z.object({
+      title: z.string(),
+      originalSource: z.object({
+        url: z.string(),
+        label: z.string(),
+      }).optional(),
+    })
+  })
 };
