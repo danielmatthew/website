4147cec68f9132c9d76ad38fab8b4b2c8e436aef
Dan Matthew <111794+danielmatthew@users.noreply.github.com>
2023-09-12 12:00:35 +0100

    fix: add draft property to projects

--- src/content/config.js
+++ src/content/config.js
@@ -10,6 +10,7 @@ export const collections = {
       tags: z.array(z.string()),
       img: z.string(),
       img_alt: z.string().optional(),
+      draft: z.boolean().optional(),
     }),
   }),
   posts: defineCollection({
